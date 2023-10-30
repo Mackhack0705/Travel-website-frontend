3453af9ead27b20825f61c71eb18aea407a6e2c2
Rajesh <malirajesh0705@gmail.com>
2023-10-30 22:28:09 +0530

    added media query in navbar

--- src/components/Nbar.jsx
+++ src/components/Nbar.jsx
@@ -3,6 +3,7 @@ import '../style/nbar.css'
 import { Link } from 'react-router-dom'
 import logo from '../images/Company-logo.png'
 import { FaUser } from 'react-icons/fa'
+import { BiSearch } from 'react-icons/bi'
 
 
 
@@ -10,28 +11,31 @@ const Nbar = ({ isLoggedIn }) => {
   return (
     <div className="container main-nav fl">
       <a href="#logo" className="company-logo">
-        <img src={logo} alt="" style={{ height: 80, width: 125 }} />
+        <img src={logo} alt="" style={{ height: "80px", width: "100%" }} />
       </a>
-      <div className="menu">
-        <ul className='fl'>
-          <li><Link to={"/Home"}>Home</Link></li>
-          <li><Link to={"/About"}>About</Link></li>
-          <li><Link to={"/Services"}>Services</Link></li>
-          <li><Link to={"/Contact us"}>Contact Us</Link></li>
-        </ul>
-      </div>
-      <div className="bt">
-        <input type="text" className='search-input' placeholder='Search' />
-        {
-          isLoggedIn ? (
-            <a href="#profile"><FaUser size={25} style={{ marginLeft: 30, color: "#ffff"} }/></a>
-          ) : (
-            <>
-              <Link to={"/signup-form"}><button className='primary-button'>Sign up</button></Link>
-              <Link to={"/login-form"}><button className="secondary-button">Login</button></Link>
-            </>
-          )
-        }
+      <div className='drop-down fl'>
+        <div className="menu">
+          <ul className='fl'>
+            <li><Link to={"/Home"}>Home</Link></li>
+            <li><Link to={"/About"}>About</Link></li>
+            <li><Link to={"/Services"}>Services</Link></li>
+            <li><Link to={"/Contact us"}>Contact Us</Link></li>
+          </ul>
+        </div>
+        <div className="bt">
+          <input type="text" className='search-input' placeholder='Search' />
+          <BiSearch style={{ color: "#eab032" }} size={25} className='search-icon' />
+          {
+            isLoggedIn ? (
+              <a href="#profile"><FaUser size={25} style={{ marginLeft: 30, color: "#ffff" }} /></a>
+            ) : (
+              <>
+                <Link to={"/signup-form"}><button className='primary-button btn-1'>Sign up</button></Link>
+                <Link to={"/login-form"}><button className="secondary-button btn-2">Login</button></Link>
+              </>
+            )
+          }
+        </div>
       </div>
     </div>
   )
