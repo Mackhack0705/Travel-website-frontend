d746d39ee33477415eeb959f9ea2df1bab0d3593
Rajesh <malirajesh0705@gmail.com>
2023-12-22 21:29:26 +0530

    remove unnecessary lines

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import "../style/tour.css";
 import tour from "../videos/tour.mp4"
 
